e2d1994e21695f6f52deb0d5dd3e1f4f1bbfbc32
Fenrir <154215093+Yggdrasilh@users.noreply.github.com>
2024-04-05 17:49:19 +0200

    Add files via upload

---
+++ Gestinnaire_domotique/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Appeler l'API pour récupérer les informations utilisateur
+        $apiUrl = $this->baseUrlApi . '/restaurant';;
+        $apiData = file_get_contents($apiUrl);
+        $restoData = json_decode($apiData, true);
+
+        // var_dump($restoData);
+
+
+        $this->render('home/index', ['restoData' => $restoData]);
+    }
+
+    public function test()
+    {
+        $this->render('home/index');
+    }
+}
